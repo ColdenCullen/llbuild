c54408e4394f985a2bb46cc3cdb77892f669caed
Colden Cullen <ColdenCullen@gmail.com>
2015-03-22 14:14:04 -0700

    Initial Windows support

--- source/llbuild/phases/compile.d
+++ source/llbuild/phases/compile.d
@@ -19,6 +19,7 @@ final class Compile : Phase, Extension!( Compile, Phase )
     {
         import std.algorithm: canFind, filter, group, map;
         import std.array: array, join, replace;
+        import std.file: exists, mkdirRecurse;
         import std.path: extension, relativePath, buildNormalizedPath;
 
         string getIntermediatePath( const string path ) pure
@@ -26,6 +27,10 @@ final class Compile : Phase, Extension!( Compile, Phase )
             auto relPath = relativePath( path, project.projectRoot ).replace( "..", "." ).buildNormalizedPath();
             return project.intermediatePath.buildNormalizedPath( relPath );
         }
+
+        // On Windows, compilers don't create the intermediate path if it doesn't exist
+        if( !project.intermediatePath.exists )
+            mkdirRecurse( project.intermediatePath );
 
         auto files = project.sourcePaths.map!( p => project.fileFinder.findFiles( p ) ).join();
 
